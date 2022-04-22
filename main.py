b7243c43df626cc1594ff8cbe11c4d7a2a58aac0
XiaoQuQu <1107439216@qq.com>
2022-04-22 17:33:34 +0800

    add copyright output

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from colorama import Fore, Style
 import command
 from config import is_section_exist, get_config, set_config
 import logging
+
+version = "0.1.1"
+is_dev_ver = True
 
 
 def logger_init():
@@ -13,10 +17,19 @@ def logger_init():
                         format="[%(levelname)s] %(message)s")
 
 
+def print_copyright():
+    print(Fore.LIGHTWHITE_EX + Style.BRIGHT + "PIC LITE SOFTWARE " + Fore.CYAN + version)
+    if is_dev_ver:
+        print(Fore.RED + "WARNING:" + Style.NORMAL + Fore.MAGENTA + "You are using the development version.")
+    
+    print(Style.RESET_ALL)
+
+
 if __name__ == "__main__":
     # logger_init()
-    strin = input("> ")
+    print_copyright()
+    strin = input(Fore.CYAN + Style.BRIGHT + "> " + Style.RESET_ALL)
     while command.is_command(strin):
         command.run_command(strin)
-        strin = input("> ")
+        strin = input(Fore.CYAN + Style.BRIGHT + "> " + Style.RESET_ALL)
     exit()
